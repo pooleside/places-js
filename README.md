9111f00ceebd0760af5694b52191c2c966a817ae
pooleside <lpoole26@gmail.com>
2015-09-21 16:25:41 -0700

    "final files, program works"

---
+++ README.md
@@ -0,0 +1,40 @@
+# _Places You've Been (JavaScript)_
+
+#### By _Leslie Poole_
+
+## Description
+A program that allows the user to enter a place they have visited along with additional details: time of year they visited, landmarks and notes.  Each new place will be added to a list of all their places.
+
+## Setup
+
+* Clone this git repository on your computer.
+* Open the count-up.html file in your browser(Chrome) to run the program.
+
+## Technologies Used
+
+JavaScript, JQuery, Mocha, Chai, Bootstrap, HTML, and CSS
+
+
+### Legal
+
+Copyright (c) 2015 _Leslie Poole_
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  $("#add-detail").click(function() {
+    $("#new-details").append('<div class="new-detail">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-time">Time of year</label>' +
+                                   '<input type="text" class="form-control new-time">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-landmark">Landmark</label>' +
+                                   '<input type="text" class="form-control new-landmark">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-note">Notes</label>' +
+                                   '<input type="text" class="form-control new-note">' +
+                                 '</div>' +
+                               '</div>');
+  });
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+
+    var newPlace = { city: inputtedCity, state: inputtedState, details: [] };
+
+    $(".new-detail").each(function() {
+      var inputtedTime = $(this).find("input.new-time").val();
+      var inputtedLandmark = $(this).find("input.new-landmark").val();
+      var inputtedNote = $(this).find("input.new-note").val();
+
+      var newDetail = { time: inputtedTime, landmark: inputtedLandmark, note: inputtedNote };
+      newPlace.details.push(newDetail);
+    });
+
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.city + "</span></li>");
+
+    $(".place").last().click(function() {
+      $("#show-place").show();
+
+      $("#show-place h2").text(newPlace.city);
+      $(".city").text(newPlace.city);
+      $(".state").text(newPlace.state);
+
+      $("ul#details").text("");
+      newPlace.details.forEach(function(detail) {
+        $("ul#details").append("<li>" + detail.time + ", " + detail.landmark + ", " + detail.note + "</li>");
+      });
+    });
+
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input.new-time").val("");
+    $("input.new-landmark").val("");
+    $("input.new-note").val("");
+  });
+});
